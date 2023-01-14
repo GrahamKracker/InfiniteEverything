c00c8cbef3bd55a04623d462a1c75cb1c08dedf0
GrahamKracker <gjguzynski@gmail.com>
2023-01-14 13:23:12 -0500

    fix up description

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -3,7 +3,7 @@
 public static class ModHelperData
 {
     public const string Name = "InfiniteEverything";
-public const string Description = "Gives you infinite Monkey Money & Monkey Knowledge on the display. DO NOT USE THIS TO CHEAT";
+public const string Description = "Gives you infinite currency on the display. DO NOT USE THIS TO CHEAT";
     public const string Version = "1.0.0";
     public const string RepoOwner = "GrahamKracker";
     public const string RepoName = "InfiniteEverything";
